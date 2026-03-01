47a05e79787722bf552e787e085ceaf8ffd809c7
Craig A. Butler <wunderhund@gmail.com>
2026-03-01 09:50:00 -0500

    temp: console debugging

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
+console.log("craigs-wounded-status: script file loaded");
 Hooks.once('init', () => {
+  console.log("loading craigs-wounded-status...");
   CONFIG.statusEffects.push({
     id: "craigs-wounded-status",
     label: "Wounded Status Effect",
     icon: "modules/craigs-wounded-status/icons/wounded.webp"
   });
+  console.log(
+    "confim craigs-wounded-status has been added",
+    CONFIG.statusEffects.find(e => e.id === "craigs-wounded-status")
+  );
 });
