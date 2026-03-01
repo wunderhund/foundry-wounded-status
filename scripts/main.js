51df2b547ecc3d34b87c69daa9af81b090a6ccb2
Craig A. Butler <wunderhund@gmail.com>
2026-03-01 09:18:19 -0500

    fix: rename module label

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 Hooks.once('init', () => {
   CONFIG.statusEffects.push({
     id: "craigs-wounded-status",
-    label: "Wounded",
+    label: "Wounded Status Effect",
     icon: "modules/craigs-wounded-status/icons/wounded.webp"
   });
 });
